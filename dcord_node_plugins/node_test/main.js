0e88aa0413b5ddde4dfca25dbbd726a46ad3a58e
DragonFire <64083584+DragonFire1230@users.noreply.github.com>
2022-05-20 13:47:57 +0500

    Update main.js

--- dcord_node_plugins/node_test/main.js
+++ dcord_node_plugins/node_test/main.js
@@ -1,2 +1,2 @@
 console.log("Node test plugin by DragonFire");
-console.log("If you run it in javascript, you got this output in developer console (Not in node console)");
+console.log("If you run it in javascript, you got this output in developer console (Not in node.js console)");
