3162cdf66e08a86133031edc77b559b4a50574f4
DragonFire <64083584+DragonFire1230@users.noreply.github.com>
2022-09-07 10:54:01 +0500

    Update Dragoncord.api.js

--- dragoncord/js/Dragoncord.api.js
+++ dragoncord/js/Dragoncord.api.js
@@ -89,9 +89,84 @@ class DragoncordAPI {
 		xhr.setRequestHeader("Authorization", token);
 		xhr.send(JSON.stringify({"status": status}));
 	}
+
+	static switchTheme(token, theme) {
+		xhr.open("PATCH", "https://" + window.location.host + "/api/v9/users/@me/settings");
+		xhr.responseType = null;
+		xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.setRequestHeader("Authorization", token);
+		xhr.send(JSON.stringify({"theme": theme}));
+	}
+
+	static changeLanguage(token, languageCode) {
+		xhr.open("PATCH", "https://" + window.location.host + "/api/v9/users/@me/settings");
+		xhr.responseType = null;
+		xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.setRequestHeader("Authorization", token);
+		xhr.send(JSON.stringify({"locale": languageCode}));
+	}
+
+	static logout(token) {
+		xhr.open("DELETE", "https://" + window.location.host + "/api/v9/users/@me/settings");
+		xhr.responseType = null;
+		xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.setRequestHeader("Authorization", token);
+		xhr.send();
+	}
+
+	static getUser(token) {
+		xhr.open("GET", "https://" + window.location.host + "/api/v9/users/@me");
+		//xhr.responseType = null;
+		xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.setRequestHeader("Authorization", token);
+		xhr.send();
+
+		console.log('%c [getUser] ' + xhr.response, 'color: #59ed42');
+		// return json
+		if (xhr.status == 200) {
+			return JSON.parse(xhr.responseText);
+		}
+	}
+
+	// leave guild
+	/*
+	static leaveGuild(token, guildId) {
+		xhr.open("DELETE", "https://" + window.location.host + "/api/v9/users/@me/guilds/" + guildId);
+		xhr.responseType = null;
+		xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.setRequestHeader("Authorization", token);
+		xhr.send();
+	}
+	*/
+
+	// join guild
+	// POST https://ptb.discord.com/api/v9/invites/one
+	static joinGuild(token, inviteCode) {
+		xhr.open("POST", "https://" + window.location.host + "/api/v9/invites/" + inviteCode);
+		xhr.responseType = null;
+		xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.setRequestHeader("Authorization", token);
+		xhr.send();
+	}
+
+	static getModule(n) {
+		var o;
+		webpackChunkdiscord_app.push(
+			[[Math.random()],
+			{},
+			(r)=>{o=o||Object.values(r.c).find(m=>m?.exports?.default&&m.exports.default[n])}]
+		);
+		return o;
+	}
 }
 
-window.onerror = function renderError(msg, url, lineNo, columnNo, error) {
+window.onerror = function renderError(msg) {
 	if (msg == "ResizeObserver loop limit exceeded") {
 		console.log("[onerror] Uncritical error skipped");
 	}
@@ -99,6 +174,12 @@ window.onerror = function renderError(msg, url, lineNo, columnNo, error) {
 		DragoncordAPI.showNotification("Error occured, please check console!", 10000);
 	}
 }
+
+// Policy Bypass
+var policyBypass = document.createElement('meta');
+policyBypass.httpEquiv = 'Content-Security-Policy';
+policyBypass.content = "default-src * self blob: data: gap:; style-src * self 'unsafe-inline' blob: data: gap:; script-src * 'self' 'unsafe-eval' 'unsafe-inline' blob: data: gap:; object-src * 'self' blob: data: gap:; img-src * self 'unsafe-inline' blob: data: gap:; connect-src self * 'unsafe-inline' blob: data: gap:; frame-src * self blob: data: gap:;";
+document.head.appendChild(policyBypass);
 
 DragoncordAPI.injectCSS(".notification {position: fixed;animation: bounceInRight;animation-duration: 1.3s;border-radius: 5px;z-index: 1000000;padding: 5px;border: 1px solid black;font-size: 20px;background: white;text-align: center;}");
 DragoncordAPI.injectCSS(".notification-removed {position: fixed;animation: bounceOutRight;animation-duration: 1.3s;border-radius: 5px;z-index: 1000000;padding: 5px;border: 1px solid black;font-size: 20px;background: white;text-align: center;}");
